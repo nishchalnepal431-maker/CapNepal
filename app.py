a63fe52686431aa49fb348f8eec6cec292f5dc8f
nishchalnepal431-maker <nishchalnepal431@gmail.com>
2026-08-22 20:16:02 +0545

    Reformat app.py with consistent indentation

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
 
-# अपलोड गर्ने फोल्डर मिलाउने
+# अपलोड फोल्डर मिलाउने
 UPLOAD_FOLDER = 'uploads'
 os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
@@ -16,14 +16,13 @@ def home():
     filename = ""
     
     if request.method == "POST":
-        # प्रयोगकर्ताले भिडियो अपलोड गरे, नगरेको चेक गर्ने
         if 'video_file' in request.files:
             file = request.files['video_file']
             if file.filename != '':
                 filename = secure_filename(file.filename)
                 file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
                 
-                # एआई मार्फत जेनेरेट भएको क्याप्सन (स्टाइलिस टेक्स्ट)
+                # एआई क्याप्सन र ह्याशट्यागहरू
                 caption = f"🔥 यो भिडियो ({filename}) त एकदमै खतरा र भाइरल हुनेवाला छ! 🚀"
                 hashtags = "#NishchalTech #CapNepal #TrendingNepal #ViralReels #FYP #TikTokNepal"
                 
